66fab210743f86f193531054909fec7e19a70d61
rakib <rakib@rakib-pc.pc>
2020-10-26 11:20:53 +0600

    side nav clear

--- LanacoinApp/views.py
+++ LanacoinApp/views.py
@@ -1,4 +1,69 @@
 from django.shortcuts import render
 
 def home_view(request):
-    return render(request,'home_page.html')
+    top_news = [
+        {
+            'title': 'Land grab.',
+            'sub_title':"Strong march in Entre Ríos after Etchevehere's complaint",
+            'details':'''Producers from the province are mobilized in Santa Elena, in front of the former official's field; they went there to reject the usurpation''',
+            'reporter': 'By Jaime Rosemberg',
+            'cover_photo':'images/recent-1.webp'
+        },
+        {
+            'title': 'Massive and mandatory. ',
+            'sub_title':"The vaccination plan that the Government plans at the end of the year",
+            'reporter': 'By Jaime Rosemberg',
+            'cover_photo':'images/3449885h186.webp',
+        },
+        {
+            'title': 'Massive and mandatory. ',
+            'sub_title':"The vaccination plan that the Government plans at the end of the year",
+            'reporter': 'By Jaime Rosemberg',
+            'cover_photo':'images/3449885h186.webp',
+        },
+        
+    ]
+    top_news_lower=[
+        {
+            'title': 'Land grab.',
+            'sub_title':"Strong march in Entre Ríos after Etchevehere's complaint",
+            'details':'''Producers from the province are mobilized in Santa Elena, in front of the former official's field; they went there to reject the usurpation''',
+            'reporter': 'By Jaime Rosemberg',
+            'cover_photo':'images/recent-1.webp'
+        },
+        {
+            'title': 'Massive and mandatory. ',
+            'sub_title':"The vaccination plan that the Government plans at the end of the year",
+            'reporter': 'By Jaime Rosemberg',
+            'cover_photo':'images/3449885h186.webp',
+        },
+        {
+            'title': 'Massive and mandatory. ',
+            'sub_title':"The vaccination plan that the Government plans at the end of the year",
+            'reporter': 'By Jaime Rosemberg',
+            'cover_photo':'images/3449885h186.webp',
+        },
+        {
+            'title': 'Land grab.',
+            'sub_title':"Strong march in Entre Ríos after Etchevehere's complaint",
+            'details':'''Producers from the province are mobilized in Santa Elena, in front of the former official's field; they went there to reject the usurpation''',
+            'reporter': 'By Jaime Rosemberg',
+            'cover_photo':'images/recent-1.webp'
+        },
+        {
+            'title': 'Massive and mandatory. ',
+            'sub_title':"The vaccination plan that the Government plans at the end of the year",
+            'reporter': 'By Jaime Rosemberg',
+            'cover_photo':'images/3449885h186.webp',
+        },
+        {
+            'title': 'Massive and mandatory. ',
+            'sub_title':"The vaccination plan that the Government plans at the end of the year",
+            'reporter': 'By Jaime Rosemberg',
+            'cover_photo':'images/3449885h186.webp',
+        },
+        
+        
+
+    ]
+    return render(request,'home_page.html',{'top_news':top_news,'top_news_lower':top_news_lower,'more_news':top_news_lower,'other_theme':top_news_lower,'interest':top_news_lower,'most_read':top_news_lower})
--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,6 @@
 function openNav() {
   document.getElementById("mySidepanel").style.width = "280px";
+  document.getElementById("mySidepanel").style.overflowX = "visible";
   document.getElementById("overlay").style.display = "block";
   window.addEventListener('scroll', noScroll)
 }
@@ -7,6 +8,7 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
   document.getElementById("overlay").style.display = "none";
+  document.getElementById("mySidepanel").style.overflowX = "hidden";
   window.removeEventListener('scroll', noScroll)
 }
 function noScroll() {
